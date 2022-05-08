1cf29cc0eeeb831113e20255008268479596520c
Pankaj Maurya <pankaj.maurya@appventurez.com>
2022-05-08 23:49:04 +0530

    server socket connect and disconnect implemented

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const path =require('path')
 
 const express = require('express')
-const app = express()
+let app = express()
 const publicPath = path.join(__dirname,'/../public')
-
+const http = require('http')
+const socketIO = require('socket.io')
 
 app.use(express.static(publicPath))
+let server = http.createServer(app)
+
+let io = socketIO(server)
+
+io.on('connection',(socket)=>{
+    console.log("New user has connected")
+
+
+    //check here to user connected or not//
+    socket.on("disconnect",()=>{
+        console.log("user was disconnected to server")
+    })
+
+})
+
 
 
 const port = process.env.PORT || 3000
-app.listen(port,()=>{
+server.listen(port,()=>{
 console.log(`server is running ${port}`)
     
 })
